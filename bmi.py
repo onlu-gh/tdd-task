7487498fee96e0856b5eea4e6335288448e3bdc9
ofekl26 <45328122+ofekl26@users.noreply.github.com>
2020-01-15 15:03:30 +0200

    get condition based on bmi
    condition test - failed.
    only normal weight option exists

--- bmi.py
+++ bmi.py
@@ -1,6 +1,9 @@
 
 def get_bmi(h,w):
     return w / (h*h)
+def get_cond(bmi):
+    if bmi >= 18.5 and bmi < 25:
+        return 'normal weight'
 
 weight = input('weight(kg): ')
 height = input('height(m): ')
