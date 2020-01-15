174662d50cad03296d6581a6d046d98f04599c35
ofekl26 <45328122+ofekl26@users.noreply.github.com>
2020-01-15 15:05:56 +0200

    get condition based on bmi
    condition test - failed.
    obesity classes conditions are missing.

--- bmi.py
+++ bmi.py
@@ -6,6 +6,8 @@ def get_cond(bmi):
         return 'normal weight'
     elif bmi <18.5:
         return 'below normal weight'
+    elif bmi >=25 and bmi < 30:
+        return 'overweight'
     
 weight = input('weight(kg): ')
 height = input('height(m): ')
